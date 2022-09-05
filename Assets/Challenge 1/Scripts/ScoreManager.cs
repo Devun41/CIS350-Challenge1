f60b7e9714aef084b16f6a207a71a6505ed55e2f
Devun Schneider <dschneider@mail.bradley.edu>
2022-09-04 20:04:04 -0500

    Finished Challenge 1

---
+++ Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public Text textbox;
+
+    private void Start()
+    {
+        gameOver = false;
+        score = 0;
+        won = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+        if (score >= 5)
+        {
+            won = true;
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You win! \n Press R to Try Again!";
+            }
+            else
+            {
+                textbox.text = "You lose!\n Press R to Try Again!";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            }
+        }
+    }
+}
---
+++ Assets/Challenge 1/Scripts/TriggerZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            //set the textbox text to You Win!
+            triggered = true;
+            ScoreManager.score++;
+        }
+    }
+}
